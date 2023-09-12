d89f4facd83e6b231fc49276a3da69e9dc4cc666
Tin Duc Ngo <tinngo@pop-os.localdomain>
2023-09-12 00:24:09 -0500

    update

--- P03/full_credit/Publication.java
+++ P03/full_credit/Publication.java
@@ -12,13 +12,25 @@ public class Publication
 
     //Constructor 
     public Publication(String title, String author, int copyright, String loanedTo, LocalDate dueDate)
-    {
+    {   
+        //Initialize fields
         this.title = title;
         this.author = author;
         this.copyright = copyright;
         this.loanedTo = loanedTo;
         this.dueDate = dueDate;
 
+        //get current year and perform data validation for copyright year
+        int currentYear = LocalDate.now().getYear();
+
+        if (copyright < 1900 || copyright > currentYear)
+        {
+            throw new IllegalArgumentException("The year has to be between 1900 and current year");
+        }
+    }
+
+    public void checkOut (String patron)
+    {
         
     }
 
