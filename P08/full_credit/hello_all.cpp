d7492c6840cc452654986173545cc1a96d63882c
Tin Ngo <tinngo@pop-os.localdomain>
2023-10-28 23:04:48 -0500

    full credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{   
+    std::string name;
+    
+    std::cout << "What's your name? ";
+    std::cin >> name;
+
+
+    std::cout << "Hello, " << name << "!" << std::endl;
+
+    return 0;
+}
---
+++ P08/full_credit/hello_me.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello, Tin!" << std::endl;
+
+    return 0;
+}
